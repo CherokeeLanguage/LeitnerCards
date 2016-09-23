f12be472477520a760f7f6271f4aa6c996d5ebec
mjoyner <mjoyner@vbservices.net>
2016-09-22 20:46:21 -0400

    add "id()"

--- LeitnerCards/src/main/java/com/cherokeelessons/deck/ICardData.java
+++ LeitnerCards/src/main/java/com/cherokeelessons/deck/ICardData.java
@@ -1,5 +1,14 @@
 package com.cherokeelessons.deck;
 
 public interface ICardData {
+	/**
+	 * A string generated from selected fields for sorting purposes.
+	 */
 	public String sortKey();
+
+	/**
+	 * A string generated from selected fields for card id purposes. Useful to
+	 * keep track of what cards are "in play" between sessions.
+	 */
+	public String id();
 }
